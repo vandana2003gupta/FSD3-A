774316ce926e0ab7329570fb65375f953ce90623
vandana2003gupta <vandana2003gupta@gmail.com>
2024-10-08 15:26:42 +0530

    Day4

---
+++ Day4/valid.js
@@ -0,0 +1,9 @@
+function validated() 
+{
+    let username = document.getElementById("fun").value;
+    let pass = document.getElementById("pass").value;
+    if(username.length<10)
+    {
+        return false;
+    }
+}
