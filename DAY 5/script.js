b1d71892d08eaf5f0cc547a4840051f5b69d8769
vandana2003gupta <vandana2003gupta@gmail.com>
2024-11-05 15:04:57 +0530

    Day7

---
+++ DAY 5/script.js
@@ -0,0 +1,14 @@
+//let x =confirm("Are you sure to logout");
+//alert(x);
+//let x =confirm("Are you sure?");
+//alert(x);
+
+//let v= prompt("enter your name","Utk")
+//alert(v);
+
+
+//let disp= () => {
+ //   console.log("welcomee to arrow functionn");
+}//
+//disp();
+
